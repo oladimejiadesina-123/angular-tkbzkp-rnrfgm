af00ce399e011514605dcb7c96a7fce7ef03e8e3
oladimejiadesina-123 <adesinaoladimeji04@gmail.com>
2022-03-25 19:21:29 +0100

    3 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -8,7 +8,7 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent implements OnInit {
 
-  @Input() product!:Product;
+  @Input() product!: Product;
 
   constructor() { }
 
